2139311b27d41be717d600ae5c973e149e66ce0f
Ramiro Rojo <ramiro.rojo.cretta@gmail.com>
2020-05-30 20:08:16 -0300

    Use better hash examples

--- test/validators/hash-validator.spec.js
+++ test/validators/hash-validator.spec.js
@@ -5,7 +5,8 @@ const expect = require("chai").expect;
 describe("HashValidator", function () {
   describe(".validate", function () {
     context("when a valid hash is validated", function () {
-      const hash = "00ajdkasj3mncmfkajhjd34";
+      const hash =
+        "0043d572a9723b7131adfa87bc05d1fa21a99a1e7de8f162ac9ce4da750e78c0";
 
       it("evaluates hash as valid", function () {
         expect(HashValidator.validate(hash)).to.eq(true);
@@ -13,7 +14,8 @@ describe("HashValidator", function () {
     });
 
     context("when an invalid hash is given", function () {
-      const hash = "laskdl3mcdjfdjsfhdsjhfjsd";
+      const hash =
+        "0843d572a9723b7131adfa87bc05d1fa21a99a1e7de8f162ac9ce4da750e78c0";
 
       it("evaluates hash as invalid", function () {
         expect(HashValidator.validate(hash)).to.eq(false);
