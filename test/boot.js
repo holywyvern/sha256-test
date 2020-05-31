0b7bc71363766dc2d9d5241f6f43d6746936e0d5
Ramiro Rojo <ramiro.rojo.cretta@gmail.com>
2020-05-30 21:11:20 -0300

    Remove file before tests

--- test/boot.js
+++ test/boot.js
@@ -1,5 +1,12 @@
 const path = require("path");
 const dotenv = require("dotenv");
+const fs = require("fs-extra");
 
 const dotEnvPath = path.join(__dirname, ".mocha-env");
 dotenv.config({ path: dotEnvPath });
+
+const { STORAGE_FILE } = require("../src/utils/config");
+
+if (fs.existsSync()) {
+  fs.removeSync(STORAGE_FILE);
+}
