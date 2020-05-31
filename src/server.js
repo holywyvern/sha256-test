0c3bad0235891065fb2791e79a9154624eca982b
Ramiro Rojo <ramiro.rojo.cretta@gmail.com>
2020-05-30 21:17:53 -0300

    Add morgan as a logger

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 const messages = require("./routes/messages");
 
 const server = express();
+if (process.env.NODE_ENV !== "test") {
+  server.use(morgan());
+}
 server.use(bodyParser.json());
 
 server.use("/api/messages", messages);
